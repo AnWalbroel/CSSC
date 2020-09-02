e98c3c8911587f245c9c4f72ed8197237b546d4b
Marek Jacob <MeraX@users.noreply.github.com>
2020-09-02 12:28:29 +0200

    adjust paths to /net/sever/

--- CSSC_main.py
+++ CSSC_main.py
@@ -27,25 +27,25 @@ from TB_statistics_raw import run_TB_statistics_raw
 # # # # # # 	explained in the README file).
 # # # # # mwr_base_path = "/data/hamp/flights/EUREC4A/"
 # # # # # day_folders = sorted(glob.glob(mwr_base_path + "*"))[0:17]	# all day folders will be saved here
-# # # # # mwr_out_path = "/work/walbroel/CSSC_test/halo/mwr/"
+# # # # # mwr_out_path = "/net/sever/walbroel/CSSC_test/halo/mwr/"
 # # # # # print("Running concat_mwr.py ..........\n")
 # # # # # run_concat_mwr(day_folders, mwr_out_path)
 
 
 
 # # # # # # 2. "dropsonde_raw_gap_filler.py":
-# # # # # path_raw_sondes = "/work/walbroel/CSSC_test/sonde_half_raw/"
-# # # # # data_out_path_halo = "/work/walbroel/CSSC_test/sonde_half_raw_v01/"
+# # # # # path_raw_sondes = "/net/sever/walbroel/CSSC_test/sonde_half_raw/"
+# # # # # out_path_halo_dropsonde = "/net/sever/walbroel/CSSC_test/sonde_qc_interpolated_v01/"
 # # # # # path_BAH_data = "/data/hamp/flights/EUREC4A/unified/"	# BAHAMAS data path
 # # # # # print("Running dropsonde_raw_gap_filler.py ..........\n")
-# # # # # run_dropsonde_raw_gap_filler(path_raw_sondes, data_out_path_halo, path_BAH_data)
+# # # # # run_dropsonde_raw_gap_filler(path_raw_sondes, out_path_halo_dropsonde, path_BAH_data)
 
 
 
 #	3. Either manually select and download the SST data from the website given in
 #	the README file or assign latitude and longitude boundaries and required dates
 #	for an automated download:
-path_sst = "/work/walbroel/CSSC_test/sst_slice/"
+path_sst = "/net/sever/walbroel/CSSC_test/sst_slice/"
 lat_bound = [0, 40]
 lon_bound = [-70, 0]
 start_date = "2020-01-19"
@@ -56,22 +56,21 @@ download_sst_data(path_sst, lat_bound, lon_bound, start_date, end_date)
 
 
 # # # # # #	4. "HALO_raw_dropsonde_to_TB.py":
-# # # # # path_halo_dropsonde = "/work/walbroel/CSSC_test/sonde_half_raw_v01/"	# interpolated dropsonde output path
-# # # # # path_sst_data = "/work/walbroel/CSSC_test/sst_slice/"		# SST data path
+# # # # # path_halo_dropsonde = out_path_halo_dropsonde	# interpolated dropsonde output path
+# # # # # path_sst = path_sst		# SST data path
 # # # # # path_BAH_data = "/data/hamp/flights/EUREC4A/unified/"	# BAHAMAS data path
-# # # # # pam_out_path = "/work/walbroel/CSSC_test/pamtra_half_raw_PQC/"
-# # # # # pamtra_datadir = '/work/walbroel/pamtra/'
-# # # # # print("Running HALO_raw_dropsonde_to_TB.py ..........\n")
 # # # # # run_HALO_raw_dropsonde_to_TB(path_halo_dropsonde, path_sst_data, path_BAH_data, pam_out_path, pamtra_datadir)
 
+# # # # # pam_out_path = "/net/sever/walbroel/CSSC_test/pam_out/"
+# # # # # print("Running HALO_raw_dropsonde_to_TB.py ..........\n")
 
 
 # # # # # #	5. "TB_statistics_raw.py":
-# # # # # path_mwr = "/work/walbroel/CSSC_test/halo/mwr/"		# path of the concatenated mwr files
-# # # # # path_pam_ds = "/work/walbroel/CSSC_test/pamtra_half_raw_PQC/"	# output path of pamtra
-# # # # # path_BAH_data = "/data/hamp/flights/EUREC4A/unified/"	# BAHAMAS data path
-# # # # # out_path = "/work/walbroel/CSSC_test/sonde_comparison_half_raw/"
-# # # # # plot_path = "/work/walbroel/CSSC_test/plots/TB_stat/"
+# # # # # path_mwr = mwr_out_path		# path of the concatenated mwr files
+# # # # # path_pam_ds = pam_out_path	# output path of pamtra
+# # # # # path_BAH_data = path_BAH_data	# BAHAMAS data path
+# # # # # out_path = "/net/sever/walbroel/CSSC_test/sonde_comparison_half_raw/"
+# # # # # plot_path = "/net/sever/walbroel/CSSC_test/plots/TB_stat/"
 # # # # # scatterplot_name = "TB_scatterplot"
 # # # # # bias_ev_plotname = "TB_abs_biasevolution"
 # # # # # output_filename = "clear_sky_sonde_comparison_ALL"
